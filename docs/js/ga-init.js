48e2b655641a747be50ef0fc5222e40220765e99
Ben Mozes <s.v.kremen@gmail.com>
2025-04-21 11:36:05 +0400

    test utm fix

---
+++ docs/js/ga-init.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-G82LVRX450', {
+  page_location: window.location.href
+});
